aee1dc11f4294539bdb50dc9e13c0f362ea16c19
sultanm <sultanm@sultanms-MacBook-Pro.local>
2023-07-06 23:23:29 +0530

    Bumping videojs version to 8.3.0

--- types.ts
+++ types.ts
@@ -1,7 +1,14 @@
-import videojs from "video.js";
+// better I don't force users create the videojs track classes rather
+// than simple javascript objects
 
+type Track = { 
+  kind?: string;
+  id?: string;
+  label?: string;
+  language?: string;
+}
 export type AudioSwitchOptions = {
-  audioTracks: Array<videojs.AudioTrackOptions & { url: string }>;
+  audioTracks: Array<Track & { url: string }>;
   syncInterval?: number;
   debugInterval?: number;
 };
